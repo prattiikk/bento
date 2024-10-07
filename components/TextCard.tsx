51fc83bc4df0ff7f4454fa82f27001d357379def
prattiikk <pvks5423@gmail.com>
2024-10-07 23:31:53 +0530

    added three dot menu icon to toggle the menu

--- components/TextCard.tsx
+++ components/TextCard.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { AlignLeft, AlignCenter, AlignRight, AlignJustify, Type } from "lucide-react"
+import { AlignLeft, AlignCenter, AlignRight, AlignJustify, Type, Ellipsis } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import LayoutItem from "@/utils/Types"
@@ -13,6 +13,7 @@ export default function TextCard({ cont, item }: { cont: string, item: LayoutIte
     const [textColor, setTextColor] = useState("#FFFFFF")
     const [showBgColors, setShowBgColors] = useState(false)
     const [showTextColors, setShowTextColors] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
 
     const [layout, setLayout] = useRecoilState(GlobalLayoutRecState)
     const setUnsavedChanges = useSetRecoilState(GlobalLayoutUnsavedChangesRecState)
@@ -72,82 +73,85 @@ export default function TextCard({ cont, item }: { cont: string, item: LayoutIte
                     placeholder="Text content"
                 />
             </div>
-            <div className="absolute mt-1 left-1/2 transform -translate-x-1/2 bg-black rounded-full shadow-lg px-2 py-1">
-                <div className="flex items-center space-x-1">
-                    <Button variant="ghost" size="icon" className="text-white h-8 w-8" onClick={() => setTextAlign("left")}>
-                        <AlignLeft className="h-4 w-4" />
-                    </Button>
-                    <Button variant="ghost" size="icon" className="text-white h-8 w-8" onClick={() => setTextAlign("center")}>
-                        <AlignCenter className="h-4 w-4" />
-                    </Button>
-                    <Button variant="ghost" size="icon" className="text-white h-8 w-8" onClick={() => setTextAlign("right")}>
-                        <AlignRight className="h-4 w-4" />
-                    </Button>
-                    <Button variant="ghost" size="icon" className="text-white h-8 w-8" onClick={() => setTextAlign("justify")}>
-                        <AlignJustify className="h-4 w-4" />
-                    </Button>
-                    <div className="relative">
-                        <Button
-                            variant="ghost"
-                            className="flex items-center text-white h-8 w-8 p-0"
-                            onClick={() => setShowBgColors(!showBgColors)}
-                        >
-                            <div className="w-4 h-4 rounded-full" style={{ backgroundColor: bgColor }}></div>
+            <button className=" absolute -bottom-6 left-2 w-6 h-6" onClick={() => setShowMenu(!showMenu)}><Ellipsis /></button>
+            {showMenu &&
+                <div className="absolute mt-1 left-1/2 transform -translate-x-1/2 bg-black rounded-full shadow-lg px-2 py-1">
+                    <div className="flex items-center space-x-1">
+                        <Button variant="ghost" size="icon" className="text-white h-8 w-8" onClick={() => setTextAlign("left")}>
+                            <AlignLeft className="h-4 w-4" />
                         </Button>
-                        {showBgColors && (
-                            <div className="absolute mt-2 left-1/2 transform -translate-x-full w-48 mb-2 p-4 bg-gray-800 rounded-2xl shadow-lg z-20">
-                                <div className="grid grid-cols-8 gap-1 mb-2">
-                                    {bgColors.map((color) => (
-                                        <Button
-                                            key={color}
-                                            className="w-4 h-4 rounded-full p-0"
-                                            style={{ backgroundColor: color }}
-                                            onClick={() => {
-                                                setBgColor(color)
-                                                setShowBgColors(false)
-                                            }}
-                                        />
-                                    ))}
-                                </div>
-                                <Input
-                                    type="text"
-                                    value={bgColor}
-                                    onChange={(e) => setBgColor(e.target.value)}
-                                    className="bg-transparent rounded-md text-white border-gray-600 text-xs h-6 px-1"
-                                />
-                            </div>
-                        )}
-                    </div>
-                    <div className="relative">
-                        <Button
-                            variant="ghost"
-                            className="flex items-center text-white h-8 w-8 p-0"
-                            onClick={() => setShowTextColors(!showTextColors)}
-                        >
-                            <Type className="h-4 w-4" />
+                        <Button variant="ghost" size="icon" className="text-white h-8 w-8" onClick={() => setTextAlign("center")}>
+                            <AlignCenter className="h-4 w-4" />
                         </Button>
-                        {showTextColors && (
-                            <div className="absolute left-1/2 transform -translate-x-0 mt-2 mb-2 p-1 bg-gray-800 rounded-2xl shadow-lg z-10">
-                                <div className="flex space-x-1">
-                                    {textColors.map((color) => (
-                                        <Button
-                                            key={color.value}
-                                            variant="ghost"
-                                            className="p-1"
-                                            onClick={() => {
-                                                setTextColor(color.value)
-                                                setShowTextColors(false)
-                                            }}
-                                        >
-                                            <div className="w-4 h-4 rounded-full" style={{ backgroundColor: color.value }}></div>
-                                        </Button>
-                                    ))}
+                        <Button variant="ghost" size="icon" className="text-white h-8 w-8" onClick={() => setTextAlign("right")}>
+                            <AlignRight className="h-4 w-4" />
+                        </Button>
+                        <Button variant="ghost" size="icon" className="text-white h-8 w-8" onClick={() => setTextAlign("justify")}>
+                            <AlignJustify className="h-4 w-4" />
+                        </Button>
+                        <div className="relative">
+                            <Button
+                                variant="ghost"
+                                className="flex items-center text-white h-8 w-8 p-0"
+                                onClick={() => setShowBgColors(!showBgColors)}
+                            >
+                                <div className="w-4 h-4 rounded-full" style={{ backgroundColor: bgColor }}></div>
+                            </Button>
+                            {showBgColors && (
+                                <div className="absolute mt-2 left-1/2 transform -translate-x-full w-48 mb-2 p-4 bg-gray-800 rounded-2xl shadow-lg z-20">
+                                    <div className="grid grid-cols-8 gap-1 mb-2">
+                                        {bgColors.map((color) => (
+                                            <Button
+                                                key={color}
+                                                className="w-4 h-4 rounded-full p-0"
+                                                style={{ backgroundColor: color }}
+                                                onClick={() => {
+                                                    setBgColor(color)
+                                                    setShowBgColors(false)
+                                                }}
+                                            />
+                                        ))}
+                                    </div>
+                                    <Input
+                                        type="text"
+                                        value={bgColor}
+                                        onChange={(e) => setBgColor(e.target.value)}
+                                        className="bg-transparent rounded-md text-white border-gray-600 text-xs h-6 px-1"
+                                    />
                                 </div>
-                            </div>
-                        )}
+                            )}
+                        </div>
+                        <div className="relative">
+                            <Button
+                                variant="ghost"
+                                className="flex items-center text-white h-8 w-8 p-0"
+                                onClick={() => setShowTextColors(!showTextColors)}
+                            >
+                                <Type className="h-4 w-4" />
+                            </Button>
+                            {showTextColors && (
+                                <div className="absolute left-1/2 transform -translate-x-0 mt-2 mb-2 p-1 bg-gray-800 rounded-2xl shadow-lg z-10">
+                                    <div className="flex space-x-1">
+                                        {textColors.map((color) => (
+                                            <Button
+                                                key={color.value}
+                                                variant="ghost"
+                                                className="p-1"
+                                                onClick={() => {
+                                                    setTextColor(color.value)
+                                                    setShowTextColors(false)
+                                                }}
+                                            >
+                                                <div className="w-4 h-4 rounded-full" style={{ backgroundColor: color.value }}></div>
+                                            </Button>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
